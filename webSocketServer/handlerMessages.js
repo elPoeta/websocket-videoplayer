130f31ce0b43904531245959a97eda058c30a823
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-02-10 11:30:30 -0300

    add hndler messages

---
+++ webSocketServer/handlerMessages.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+const handlerMessages = (clients, ws, message) =>{
+  const connected = () => {
+    fs.readFile(__dirname+'/assets/movie.mp4', (err,data) => {
+      if(err){
+        console.log(`Read file error :: ${err}`);
+        return;
+      }
+      ws.send(data,{ binary:true });
+    });
+  }
+
+  const play = () => {
+
+  }
+
+  const pause = () => {
+    
+  }
+
+  const broadcast = () => {
+
+  }
+  
+  const handler = {
+     'connected': connected(),
+     'play': play(),
+     'pause': pause(),
+     'default': () => ws.send('invalid message')
+  };
+
+  return handler[message] || handler['default'];
+}
+
+module.exports = handlerMessages;
--- webSocketServer/wsServer.js
+++ webSocketServer/wsServer.js
@@ -1,11 +1,12 @@
 const WebSocket = require('ws');
+const handlerMessages = require('./handlerMessages');
 
 module.exports = server =>{
   const wss = new WebSocket.Server({ server });
   wss.on('connection', ws => {
     ws.on('message', message =>{
       console.log(`received : ${message}`);
-       ws.send('ok')
+       handlerMessages(wss.clients,ws,message);
     });
   });
   
